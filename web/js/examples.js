eabb111c43ca0a26b836ae2e2bbe47425fe32658
lmh <ksg9745@en-sys.co.kr>
2023-10-15 01:22:10 +0900

    #메인페이지

--- web/js/examples.js
+++ web/js/examples.js
@@ -7,63 +7,11 @@ $(document).ready(function () {
     $(this).removeClass("active");
   });
 
-  // 새로운 아이템 추가 함수
-  function addItem() {
-    var newItem = `
-    <li>
-      <div class="real_time_item">
-          <p>[비밀글] 강아지를 찾고 싶은데, 잃어버린 곳은 서울 구로구 근처쯤인데요.</p>
-          <ul>
-              <li>김*우</li>
-              <li>2023-07-18</li>
-          </ul>
-      </div>
-    </li>
-  `;
 
-    // 첫 번째 아이템 애니메이션 적용 (위로 이동)
-    $(".real_time_list li:first").animate(
-      { marginTop: "-70px" },
-      1000,
-      function () {
-        $(this).remove(); // 애니메이션 완료 후 삭제
-      }
-    );
 
-    // 새로운 아이템 추가 (맨 아래에)
-    $(".real_time_list").append(newItem);
-  }
 
-  // 일정 간격으로 addItem 함수 호출 (예: 3초마다)
-  setInterval(addItem, 3000);
 
-  // PC
-  // 새로운 아이템 추가 함수
-  function pcAddItem() {
-    var newItem = `
-    <li>
-      <dl>
-          <dt>가족분쟁으로 인한 상담건입니다.</dt>
-          <dd>2023-07-18</dd>
-      </dl>
-  </li>
-    `;
 
-    // 첫 번째 아이템 애니메이션 적용 (위로 이동)
-    $(".pc_real_time_list li:first").animate(
-      { marginTop: "-21px" },
-      1000,
-      function () {
-        $(this).remove(); // 애니메이션 완료 후 삭제
-      }
-    );
-
-    // 새로운 아이템 추가 (맨 아래에)
-    $(".pc_real_time_list").append(newItem);
-  }
-
-  // 일정 간격으로 addItem 함수 호출 (예: 3초마다)
-  setInterval(pcAddItem, 3000);
 
   var detail_img_01 = new Swiper(".detail_img_01", {
     spaceBetween: 10,
